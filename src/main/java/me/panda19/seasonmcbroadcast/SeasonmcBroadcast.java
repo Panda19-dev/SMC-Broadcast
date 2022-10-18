42e61b68bbacc83a527fcc23f778b1270448e517
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 23:57:14 +0200

    initial commit

--- src/main/java/me/panda19/seasonmcbroadcast/SeasonmcBroadcast.java
+++ src/main/java/me/panda19/seasonmcbroadcast/SeasonmcBroadcast.java
@@ -11,6 +11,8 @@ public final class SeasonmcBroadcast extends Plugin {
         return super.getLogger();
     }
 
+    //https://www.spigotmc.org/wiki/creating-basic-command-functions-in-bungeecord/
+    //https://www.spigotmc.org/threads/broadcast-to-the-all-network.439679/
     @Override
     public void onEnable() {
         // Plugin startup logic
